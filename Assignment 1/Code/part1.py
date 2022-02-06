23b221195c82c485037df4217203fb08b927c904
Sebastian <sebskogen@gmail.com>
2022-02-06 17:23:39 +0100

    1b

--- Assignment 1/Code/part1.py
+++ Assignment 1/Code/part1.py
@@ -79,10 +79,93 @@ data_block = data_block.reshape((8, 8))
 
 print(data_block)
 
+q_table = np.array(
+    [
+        16,
+        11,
+        10,
+        16,
+        24,
+        40,
+        51,
+        61,
+        12,
+        12,
+        14,
+        19,
+        26,
+        58,
+        60,
+        55,
+        14,
+        13,
+        16,
+        24,
+        40,
+        57,
+        69,
+        56,
+        14,
+        17,
+        22,
+        29,
+        51,
+        87,
+        80,
+        62,
+        18,
+        22,
+        37,
+        56,
+        68,
+        109,
+        103,
+        77,
+        24,
+        35,
+        55,
+        64,
+        81,
+        104,
+        113,
+        92,
+        49,
+        64,
+        78,
+        87,
+        103,
+        121,
+        120,
+        101,
+        72,
+        92,
+        95,
+        98,
+        112,
+        100,
+        103,
+        99,
+    ]
+)
+q_table = q_table.reshape((8, 8))
+
+
 data_block_DCT2 = dct2(data_block)
 
 print(data_block_DCT2)
-np.savetxt("data_block_DCT2.csv", (data_block_DCT2), fmt="%d")
+np.savetxt(
+    "data_block_DCT2.csv", (data_block_DCT2), fmt="%d"
+)  # used for tabular in latex with generator
 
-plt.imshow(np.log(np.abs(data_block_DCT2)))
+# plt.imshow(np.log(np.abs(data_block_DCT2)))
+# plt.show()
+
+
+DCT2_quantized = np.floor(data_block_DCT2 / q_table + 0.5)
+print(DCT2_quantized)
+np.savetxt(
+    "DCT2_quantized.csv", (DCT2_quantized), fmt="%d"
+)  # used for tabular in latex with generator
+
+plt.imshow(DCT2_quantized)
 plt.show()
